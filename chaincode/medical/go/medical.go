25fae0e1901afc3be8aa07e9ecd358edb47b829c
zhyulong <zhyulong.ibm.com>
2019-01-07 16:22:50 +0800

    add fun name

--- chaincode/medical/go/medical.go
+++ chaincode/medical/go/medical.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 
@@ -14,58 +13,95 @@ var logger = shim.NewLogger("电子病历")
 type SimpleChaincode struct {
 }
 
-// 姓名	性别	年龄	婚姻状况	   职业	  工作单位	 普通数据
-// name	sex	   age	 marry_status	job	  employer	common_message
-// 身份证号	          出生地	   民族	        入院日期	    陈述者	      现住址
-// identity_number	birth_place	nationality	admission_date	presenter	Current_address
-
-type Privace_data struct {
-	Name            string `json:"Name"`
-	Sex             string `json:"Sex"`
-	Age             string `json:"Age"`
-	Marry_status    string `json:"Marry_status"`
-	Job             string `json:"Job"`
-	Employer        string `json:"Employer"`
-	Identity_number string `json:"Identity_number"`
-	Birth_place     string `json:"Birth_place"`
-	Nationality     string `json:"Nationality"`
-	Admission_date  string `json:"Admission_date"`
-	Presenter       string `json:"Presenter"`
-	Common_message  string `json:"Common_message"`
+//患者基本信息数据
+type Patient_Basic_Info struct {
+	Patient_Num                    string `json:"Patient_Num"`                    //患者编号
+	Patient_Name                   string `json:"Patient_Name"`                   //患者姓名
+	Patient_Identity_Number        string `json:"Patient_Identity_Number"`        //患者身份证号
+	Patient_Sex                    string `json:"Patient_Sex"`                    //患者性别
+	Patient_Birth_Date             string `json:"Patient_Birth_Date"`             //患者出生日期
+	Patient_Nationality            string `json:"Patient_Nationality"`            //患者民族
+	Patient_Blood_Group            string `json:"Patient_Blood_Group"`            //患者血型：
+	Patient_Job                    string `json:"Patient_Job"`                    //患者职业
+	Patient_Employer               string `json:"Patient_Employer"`               //患者工作单位
+	Patient_Tel                    string `json:"Patient_Tel"`                    //患者电话
+	Patient_Relative_Name          string `json:"Patient_Relative_Name"`          //患者亲友姓名
+	Patient_Relative_Tel           string `json:"Patient_Relative_Tel"`           //患者亲友电话
+	Patient_Marriage_Status        string `json:"Patient_Marriage_Status"`        //患者婚姻状况
+	Patient_Fertility_Status       string `json:"Patient_Fertility_Status"`       //患者生育状况
+	Patient_Fertility_Age          string `json:"Patient_Fertility_Age"`          //患者生育年龄
+	Patient_Son_Num                string `json:"Patient_Son_Num"`                //患者儿子数量
+	Patient_Girl_Num               string `json:"Patient_Girl_Num"`               //患者女儿数量
+	Patient_Medical_History        string `json:"Patient_Medical_History"`        //患者病史
+	Patient_Smoke_History          string `json:"Patient_Smoke_History"`          //患者吸烟史
+	Patient_Family_Genetic_History string `json:"Patient_Family_Genetic_History"` //患者家族遗传史
 }
 
-// 主述	            现病史	                  既往史
-// main_statement	current_medical_history	past_history
-// 个人史	          生育史	      婚姻史
-// personal_history	birth_history	marriage_history
-
-type Common_data struct {
-	Identity_number         string `json:"Identity_number"`
-	Main_statement          string `json:"Main_statement"`
-	Current_medical_history string `json:"Current_medical_history"`
-	Past_history            string `json:"Past_history"`
-	Personal_history        string `json:"Personal_history"`
-	Birth_history           string `json:"Birth_history"`
-	Marriage_history        string `json:"Marriage_history"`
+//住院信息表
+type Resident_Info struct {
+	Resident_Num              string `json:"Resident_Num"`              // 住院流水号
+	Resident_Name             string `json:"Resident_Name"`             // 住院者姓名
+	Resident_Patient_Num      string `json:"Resident_Patient_Num"`      // 患者编号
+	Resident_Department       string `json:"Resident_Department"`       // 就诊科室
+	Resident_Bed_Num          string `json:"Resident_Bed_Num"`          // 床位号
+	Resident_Supervisor       string `json:"Resident_Supervisor"`       // 主管医生：
+	Resident_Supervisor_Nurse string `json:"Resident_Supervisor_Nurse"` //主管护士
+	Resident_In_Date          string `json:"Resident_In_Date"`          //住院日期
+	Resident_Out_Date         string `json:"Resident_Out_Date"`         //出院日期
+	Resident_Attention        string `json:"Resident_Attention"`        //备注
 }
 
-// 体温	脉搏	心率	血压	发育	营养
-// T	P	   R	  BP	growth	nutrition
-// 面容	 表情	      体位	      神志	               其他
-// face	expression	position	consciousness		others
+//电子病历信息
+type Record_Info struct {
+	Record_Num           string `json:"Record_Num"`           //文档编号
+	Record_Resident_Num  string `json:"Record_Resident_Num"`  //住院流水号：
+	Record_Name          string `json:"Record_Name"`          //文档名称
+	Record_Create_Doctor string `json:"Record_Create_Doctor"` //创建医生
+	Record_Create_Time   string `json:"Record_Create_Time"`   //创建时间
+	Record_Path          string `json:"Record_Path"`          //文档路径
+	Record_Hash_Value    string `json:"Record_Hash_Value"`    //文档哈希值
+	Record_Attention     string `json:"Record_Attention"`     //备注
+}
 
-type Examination_table struct {
-	Identity_number string `json:"Identity_number"`
-	T               string `json:"T"`
-	P               string `json:"P"`
-	R               string `json:"R"`
-	BP              string `json:"BP"`
-	Growth          string `json:"Growth"`
-	Nutrition       string `json:"Nutrition"`
-	Face            string `json:"Face"`
-	Expression      string `json:"Expression"`
-	Position        string `json:"position"`
-	Consciousness   string `json:"Consciousness"`
+//临床路径
+type Clinical_Pathway struct {
+	Clinical_Pathway_Num       string `json:"Clinical_Pathway_Num"`       //临床路径编号
+	Clinical_Pathway_Name      string `json:"Clinical_Pathway_Name"`      //临床路径名称：
+	Clinical_Pathway_Attention string `json:"Clinical_Pathway_Attention"` //备注
+}
+
+//临床路径项
+type Clinical_Pathway_Item struct {
+	Clinical_Pathway_Item_Num       string `json:"Clinical_Pathway_Item_Num"`       //临床路径项编号：
+	Clinical_Pathway_Item_Name      string `json:"Clinical_Pathway_Item_Name"`      //临床路径项名称：
+	Clinical_Pathway_Item_Attention string `json:"Clinical_Pathway_Item_Attention"` //备注
+}
+
+// 患者临床路径
+type Patient_Clinical_Pathway struct {
+	Patient_Clinical_Pathway_Num       string `json:"Patient_Clinical_Pathway_Num"`       //患者临床路径编号
+	Clinical_Pathway_Num               string `json:"Clinical_Pathway_Num"`               //临床路径编号
+	Patient_Clinical_Begin_Time        string `json :"Patient_Clinical_Begin_Time"`       //项目开始时间
+	Resident_Num                       string `json:"Resident_Num"`                       //住院流水号
+	Patient_Clinical_Pathway_Creator   string `json:"Patient_Clinical_Pathway_Creator"`   //创建医生
+	Patient_Clinical_Pathway_Attention string `json:"Patient_Clinical_Pathway_Attention"` //备注
+}
+
+//临床路径与临床路径项关系：Clinical_Relation
+type Clinical_Relation struct {
+	Clinical_Pathway_Num        string `json:"Clinical_Pathway_Num"`        //临床路径编号
+	Clinical_Pathway_Item_Num   string `json:"Clinical_Pathway_Item_Num"`   //临床路径项编号
+	Clinical_Relation_Attention string `json:"Clinical_Relation_Attention"` //备注
+}
+
+//临床路径执行情况
+type Clinical_Pathway_Execu_Status struct {
+	Clinical_Pathway_Execu_Num       string `json:"Clinical_Pathway_Execu_Num"`       //临床路径执行编号
+	Clinical_Pathway_Item_Num        string `json:"Clinical_Pathway_Item_Num"`        //临床路径项编号
+	Clinical_Pathway_Execu_Time      string `json:"Clinical_Pathway_Execu_Time"`      //执行时间
+	Clinical_Pathway_Execu_Status    string `json:"Clinical_Pathway_Execu_Status"`    //完成情况
+	Clinical_Pathway_Execu_Attention string `json:"Clinical_Pathway_Execu_Attention"` //备注
+
 }
 
 const InvalidNumArgs = "参数数量错误"
@@ -102,220 +138,124 @@ func (sc *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 
 func (sc *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fun, args := stub.GetFunctionAndParameters()
-	if fun == "AddPrivateData" {
-		return sc.AddPrivateData(stub, args)
-	} else if fun == "AddCommomData" {
-		return sc.AddCommomData(stub, args)
-	} else if fun == "AddExaminationData" {
-		return sc.AddExaminationData(stub, args)
-	} else if fun == "QueryPrivateData" {
-		return sc.QueryPrivateData(stub, args)
-	} else if fun == "QueryExaminationData" {
-		return sc.QueryExaminationData(stub, args)
-	} else if fun == "QueryCommonData" {
-		return sc.QueryCommonData(stub, args)
-
+	if fun == "add_Patient_Basic_Info" {
+		return sc.add_Patient_Basic_Info(stub, args)
+	} else if fun == "query_Patient_Basic_Info" {
+		return sc.query_Patient_Basic_Info(stub, args)
+	} else if fun == "add_Resident_Info" {
+		return sc.add_Resident_Info(stub, args)
+	} else if fun == "query_Resident_Info" {
+		return sc.query_Resident_Info(stub, args)
+	} else if fun == "add_Record_Info" {
+		return sc.add_Record_Info(stub, args)
+	} else if fun == "query_Record_Info" {
+		return sc.query_Record_Info(stub, args)
+	} else if fun == "add_Clinical_Pathway" {
+		return sc.add_Clinical_Pathway(stub, args)
+	} else if fun == "query_Clinical_Pathway" {
+		return sc.query_Clinical_Pathway(stub, args)
+	} else if fun == "add_Clinical_Pathway_Item" {
+		return sc.add_Clinical_Pathway_Item(stub, args)
+	} else if fun == "query_Clinical_Pathway_Item" {
+		return sc.query_Clinical_Pathway_Item(stub, args)
+	} else if fun == "add_Patient_Clinical_Pathway" {
+		return sc.add_Patient_Clinical_Pathway(stub, args)
+	} else if fun == "query_Patient_Clinical_Pathway" {
+		return sc.query_Patient_Clinical_Pathway(stub, args)
+	} else if fun == "add_Clinical_Relation" {
+		return sc.add_Clinical_Relation(stub, args)
+	} else if fun == "query_Clinical_Relation" {
+		return sc.query_Clinical_Relation(stub, args)
+	} else if fun == "add_Clinical_Pathway_Execu_Status" {
+		return sc.add_Clinical_Pathway_Execu_Status(stub, args)
+	} else if fun == "query_Clinical_Pathway_Execu_Status" {
+		return sc.query_Clinical_Pathway_Execu_Status(stub, args)
 	}
 
 	return shim.Success(nil)
 
 }
 
-//将隐私数据存入到区块中
-// Name Sex Age  Marry_status  Job   Employer  Identity_number Birth_place  Nationality   Admission_date  Presenter  Common_message
-func (sc *SimpleChaincode) AddPrivateData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 12 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	var err error
+//添加病人基本信息
+func (sc *SimpleChaincode) add_Patient_Basic_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	var private_data Privace_data
-	private_data.Name = args[0]
-	private_data.Sex = args[1]
-	private_data.Age = args[2]
-	private_data.Marry_status = args[3]
-	private_data.Job = args[4]
-	private_data.Employer = args[5]
-	private_data.Identity_number = args[6]
-	private_data.Birth_place = args[7]
-	private_data.Nationality = args[8]
-	private_data.Admission_date = args[9]
-	private_data.Presenter = args[10]
-	private_data.Common_message = args[11]
-	private_b, err := json.Marshal(private_data)
-	if err != nil {
-		return shim.Error(getErrReason(MarshalFailed, "0"))
-	}
-	private_key, err := stub.CreateCompositeKey("PrivateData", []string{private_data.Identity_number})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-	err = stub.PutPrivateData("collectionRecordPrivateDetails", private_key, private_b)
-	if err != nil {
-		return shim.Error(getErrReason(SaveStubFailed, "0"))
-	}
-	return shim.Success(getRetReason(SaveBlockSuc, "6"))
+	return shim.Success(nil)
 }
 
-//将普通数据存入区块和mysql数据库中
+//查询病人基本信息
+func (sc *SimpleChaincode) query_Patient_Basic_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-// Identity_number  Main_statement  Current_medical_history Past_history   Personal_history    Birth_history     Marriage_history
-func (sc *SimpleChaincode) AddCommomData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 7 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	var err error
-	var common_data Common_data
-	common_data.Identity_number = args[0]
-	common_data.Main_statement = args[1]
-	common_data.Current_medical_history = args[2]
-	common_data.Past_history = args[3]
-	common_data.Personal_history = args[4]
-	common_data.Birth_history = args[5]
-	common_data.Marriage_history = args[6]
-	commom_b, err := json.Marshal(common_data)
-	if err != nil {
-		return shim.Error(getErrReason(MarshalFailed, "0"))
-	}
-
-	h := sha256.New()
-	h.Write(commom_b)
-	bs := h.Sum(nil)
-
-	logger.Infof("===========commom_b======hashvalue========k=%s", bs)
-	common_key, err := stub.CreateCompositeKey("CommomData", []string{common_data.Identity_number})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-	err = stub.PutPrivateData("collectionRecord", common_key, commom_b)
-	if err != nil {
-		return shim.Error(getErrReason(SaveStubFailed, "0"))
-	}
-
-	return shim.Success(getRetReason(SaveBlockSuc, "1"))
-
+	return shim.Success(nil)
 }
 
-//将体检数据存入到区块和数据库中
-
-// Identity_number T  P   R  BP  Growth Nutrition  Face   Expression  Position  Consciousness
-func (sc *SimpleChaincode) AddExaminationData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var err error
-	var examination_data Examination_table
-	if len(args) != 11 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	examination_data.Identity_number = args[0]
-	examination_data.T = args[1]
-	examination_data.P = args[2]
-	examination_data.R = args[3]
-	examination_data.BP = args[4]
-	examination_data.Growth = args[5]
-	examination_data.Nutrition = args[6]
-	examination_data.Face = args[7]
-	examination_data.Expression = args[8]
-	examination_data.Position = args[9]
-	examination_data.Consciousness = args[10]
-	examination_b, err := json.Marshal(examination_data)
-	if err != nil {
-		return shim.Error(getErrReason(MarshalFailed, "0"))
-	}
-	h := sha256.New()
-	h.Write(examination_b)
-	bs := h.Sum(nil)
-
-	logger.Infof("===========examination_b======hashvalue========k=%s", bs)
-
-	examination_key, err := stub.CreateCompositeKey("ExaminationData", []string{examination_data.Identity_number})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-	err = stub.PutPrivateData("collectionRecord", examination_key, examination_b)
-	if err != nil {
-		return shim.Error(getErrReason(SaveStubFailed, "0"))
-	}
-
-	return shim.Success(getRetReason(SaveBlockSuc, "1"))
+//添加住院信息Resident_Info
+func (sc *SimpleChaincode) add_Resident_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
 }
 
-//args:身份证号
-func (sc *SimpleChaincode) QueryPrivateData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var err error
-	var privateData Privace_data
-
-	if len(args) != 1 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	logger.Infof("===========args========k=%s", args[0])
-
-	//从区块链中取出隐私数据
-	private_k, err := stub.CreateCompositeKey("PrivateData", []string{args[0]})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-
-	logger.Infof("===========private_k========k=%s", private_k)
-	private_b, err := stub.GetPrivateData("collectionRecordPrivateDetails", private_k)
-	if err != nil {
-		return shim.Error(getErrReason(GetDataFBlock, "0"))
-	}
-
-	logger.Infof("===========private_k 1========k=%s", string(private_b[:]))
-	err = json.Unmarshal(private_b, &privateData)
-	if err != nil {
-		return shim.Error(getErrReason(UnmarshlFailed, "0"))
-	}
-
-	return shim.Success(private_b)
+//查询住院信息
+func (sc *SimpleChaincode) query_Resident_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
 }
 
-//查询普通数据 args:身份证号
-func (sc *SimpleChaincode) QueryCommonData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var commonData Common_data
-	var err error
-	if len(args) != 1 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	commom_k, err := stub.CreateCompositeKey("CommomData", []string{args[0]})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-	logger.Infof("===========commom_k========k=%s", commom_k)
-	common_b, err := stub.GetPrivateData("collectionRecord", commom_k)
-
-	if err != nil {
-		return shim.Error(getErrReason(GetDataFBlock, "0"))
-	}
-
-	err = json.Unmarshal(common_b, &commonData)
-	if err != nil {
-		return shim.Error(getErrReason(UnmarshlFailed, "0"))
-	}
-	return shim.Success(common_b)
+//添加电子病历信息
+func (sc *SimpleChaincode) add_Record_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
 }
 
-//查询体健数据 args:身份证号
-func (sc *SimpleChaincode) QueryExaminationData(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var examinationData Examination_table
-	var err error
-	if len(args) != 1 {
-		return shim.Error(getErrReason(InvalidNumArgs, "0"))
-	}
-	examination_k, err := stub.CreateCompositeKey("ExaminationData", []string{args[0]})
-	if err != nil {
-		return shim.Error(getErrReason(CreateKey, "0"))
-	}
-	logger.Infof("===========examination_k========k=%s", examination_k)
+//查询电子病历信息
+func (sc *SimpleChaincode) query_Record_Info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
 
-	examination_b, err := stub.GetPrivateData("collectionRecord", examination_k)
-	if err != nil {
-		return shim.Error(getErrReason(GetDataFBlock, "0"))
-	}
-	err = json.Unmarshal(examination_b, &examinationData)
-	if err != nil {
-		return shim.Error(getErrReason(UnmarshlFailed, "0"))
-	}
-	return shim.Success(examination_b)
+//添加临床路
+func (sc *SimpleChaincode) add_Clinical_Pathway(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//查询临床路径
+func (sc *SimpleChaincode) query_Clinical_Pathway(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//添加临床路径项
+func (sc *SimpleChaincode) add_Clinical_Pathway_Item(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//查询临床路径项
+func (sc *SimpleChaincode) query_Clinical_Pathway_Item(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//添加患者临床路径
+func (sc *SimpleChaincode) add_Patient_Clinical_Pathway(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//查询患者临床路径
+func (sc *SimpleChaincode) query_Patient_Clinical_Pathway(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//添加临床与临床路径项的关系
+func (sc *SimpleChaincode) add_Clinical_Relation(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//查询临床与临床路径项的关系
+func (sc *SimpleChaincode) query_Clinical_Relation(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//添加临床路径执行情况
+func (sc *SimpleChaincode) add_Clinical_Pathway_Execu_Status(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+//查询临床路径执行情况
+func (sc *SimpleChaincode) query_Clinical_Pathway_Execu_Status(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
 }
 
 func main() {
