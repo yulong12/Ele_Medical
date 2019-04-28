f490085869b861b99c51a5653c21ff8a0a698b3b
zhyulong <zhyulong.ibm.com>
2019-04-28 15:02:21 +0800

    moke data

--- src/components/cash_system/registerManage/registerManage.vue
+++ src/components/cash_system/registerManage/registerManage.vue
@@ -2,7 +2,7 @@
   <div class="registerManage">
     <!-- 搜索条件 -->
     <div class="filter-container">
-      <el-input placeholder="处方编号" v-model="searchNumber"></el-input>
+      <el-input placeholder="挂号序号" v-model="searchNumber"></el-input>
       <el-input placeholder="患者姓名" v-model="searchName"></el-input>
       <el-button type="primary" icon="search" @click="search">搜索</el-button>
       <el-button type="primary" icon="view" @click="showAll">显示全部</el-button>
@@ -13,13 +13,13 @@
     <el-table :data="patients" border style="width: 100%; text-align:left">
       <el-table-column prop="number" label="挂号序号"></el-table-column>
       <el-table-column prop="name" label="患者姓名"></el-table-column>
-      <el-table-column prop="" label="挂号凭证"></el-table-column>
-      <el-table-column prop="" label="凭证号"></el-table-column>
+      <el-table-column prop="registerKind" label="挂号凭证"></el-table-column>
+      <el-table-column prop="idCard" label="凭证号"></el-table-column>
       <el-table-column prop="disease" label="科室"></el-table-column>
       <el-table-column prop="doctor" label="就诊医生"></el-table-column>
       <el-table-column prop="date" label="就诊时间"></el-table-column>
-      <el-table-column prop="" label="候诊号"></el-table-column>
-      <el-table-column prop="" label="挂号费"></el-table-column>
+      <el-table-column prop="waitingNo" label="候诊号"></el-table-column>
+      <el-table-column prop="fee" label="挂号费"></el-table-column>
       <el-table-column label="操作" width="180">
         <template scope="scope">
           <el-button type="primary" class="btn" @click="checkDetail(scope.$index)">查看</el-button>
@@ -32,16 +32,16 @@
       <ul class="patientsdetail">
         <li><h5>挂号序号：</h5><span>{{patientsdetails.number}}</span></li>
         <li><h5>患者姓名:</h5><span>{{patientsdetails.name}}</span></li>
-        <li><h5>挂号凭证:</h5><span></span></li>
-        <li><h5>凭证号：</h5><span></span></li>
+        <li><h5>挂号凭证:</h5><span>{{patientsdetails.registerKind}}</span></li>
+        <li><h5>凭证号：</h5><span>{{patientsdetails.idCard}}</span></li>
         <li><h5>性别:</h5><span>{{patientsdetails.sex}}</span></li>
         <li><h5>年龄:</h5><span>{{patientsdetails.age}}</span></li>
-        <li><h5>家庭地址:</h5><span>{{patientsdetails.position}}</span></li>
+        <li><h5>家庭地址:</h5><span>{{patientsdetails.address}}</span></li>
         <li><h5>科室:</h5><span>{{patientsdetails.disease}}</span></li>
         <li><h5>就诊医生:</h5><span>{{patientsdetails.doctor}}</span></li>
         <li><h5>就诊时间:</h5><span>{{patientsdetails.date}}</span></li>
-        <li><h5>候诊号:</h5><span></span></li>
-        <li><h5>挂号费:</h5><span></span></li>
+        <li><h5>候诊号:</h5><span>{{patientsdetails.waitingNo}}</span></li>
+        <li><h5>挂号费:</h5><span>{{patientsdetails.fee}}</span></li>
       </ul>
       <h5 slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
